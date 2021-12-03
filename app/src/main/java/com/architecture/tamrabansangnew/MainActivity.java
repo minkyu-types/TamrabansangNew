cc695f4b3fce7ae5310c6dcfdd1e7bff589d4b27
its-mingyu <lion9638@naver.com>
2021-12-04 00:11:03 +0900

    Let's do renewal

---
+++ app/src/main/java/com/architecture/tamrabansangnew/MainActivity.java
@@ -0,0 +1,18 @@
+package com.architecture.tamrabansangnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
